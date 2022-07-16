cdedecb512b14d8e247240d053842059c4dfefed
Weslleyw10 <weslleylopes.dev@gmail.com>
2022-07-16 04:33:55 -0300

    Auth finished

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -3,10 +3,77 @@ import { app } from '../../app'
 
 it('should be returns a 201 on succesful signup', async () => {
     return request(app)
-        .post('/api/users/signin')
+        .post('/api/users/signup')
         .send({
             email: 'test@test.com',
-            password: '12345678'
+            password: '1234'
         })
-        .expect(201)
+        .expect(201)        
+})
+
+it('should be returns a 400 with an invalid email', async () => {
+    return request(app)
+        .post('/api/users/signup')
+        .send({
+            email: 'testtest.com',
+            password: '1234'
+        })
+        .expect(400)        
+})
+
+it('should be returns a 400 with an invalid password', async () => {
+    return request(app)
+        .post('/api/users/signup')
+        .send({
+            email: 'test@test.com',
+            password: ''
+        })
+        .expect(400)        
+})
+
+it('should be returns a 400 with missing email and password', async () => {
+    await request(app)
+        .post('/api/users/signup')
+        .send({
+            email: 'test@test.com'
+        })
+        .expect(400) 
+        
+    await request(app)
+        .post('/api/users/signup')
+        .send({
+            password: '123456'
+        })
+        .expect(400)
+})
+
+it('should be disallow duplicate emails', async () => {
+    await request(app)
+        .post('/api/users/signup')
+        .send({
+            email: 'test@test.com',
+            password: '123456'
+        })
+        .expect(201)   
+        
+    await request(app)
+        .post('/api/users/signup')
+        .send({
+            email: 'test@test.com',
+            password: '123456'
+        })
+        .expect(400)
+})
+
+it('should be sets a cookie after succesful signup', async () => {
+    const response = await request(app)
+        .post('/api/users/signup')
+        .send({
+            email: 'test@test.com',
+            password: '123456'
+        })
+        .expect(201)      
+        
+        expect(response.get('Set-Cookie')).toBeDefined();
+    
 })
